a243413198795de17e38b7386b9e116c3dbbd69a
carlos verza <gverza88@gmail.com>
2024-11-28 19:58:24 -0300

    startt

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import userRoutes from './routes/userRoutes';
 import productsRoutes from './routes/productsRoutes'
 import cors from 'cors';
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
  app.use(cors());
  app.use('/auth', userRoutes);
 app.use('/products', productsRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+app.listen(port, () => {
+  console.log('Servidor corriendo en el puerto'+port);
 });
